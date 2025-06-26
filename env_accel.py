df788cc74d7f0ef68300c5d894d33e36dc564217
Joachim Bienvenüe <pjoi@passoire.fr>
2025-06-26 16:44:16 +0200

    try a lot of things, like making the bot spawn closer to the goal or an heuristic solver, but nothing is working (apart from the physic)

--- env_accel.py
+++ env_accel.py
@@ -16,12 +16,14 @@ def get_arr(*arr):
 class Car(gymnasium.Env):
     maxSpeed = 5
     maxi = 250
-    dist = 120
-    radius = 50
-    refresh = 0.02*7
+    dist = 300
+    radius = 55
+    refresh = 0.02
     eps = 1e+1
     epsAngle = 0.1
-    def __init__(self, render_mode=None, id=None):
+    limitStep = 10**4
+    stLengthMax = 50
+    def __init__(self, render_mode=None, id=None, every=100):
         self.mimax = self.maxi/2
         assert render_mode is None or render_mode == 'human', f"Car env dosen't support the {render_mode} rendering"
         self.render_mode = False
@@ -29,9 +31,8 @@ class Car(gymnasium.Env):
         self.render_mode = render_mode == 'human'
         self.W1 = self.dist+0j
         self.W2 = -self.dist+0j
-        self.observation_space = gymnasium.spaces.Box(low=get_arr(-1, -1, -1, -1, -1), high=get_arr(1, 1, 1, 1, 1), shape=(5,))
+        self.observation_space = gymnasium.spaces.Box(low=get_arr(-1, -1, -1, -1), high=get_arr(1, 1, 1, 1), shape=(4,))
         self.action_space = gymnasium.spaces.Box(low = get_arr(-1, -1), high=get_arr(1, 1), shape=(2,))
-        self.limitStep = 10**3
         if self.render_mode:
             self.init_rendering()
         self.datas = [[], []]
@@ -40,6 +41,7 @@ class Car(gymnasium.Env):
             with open(self.logfile, 'w') as f:f.write('')
         else:
             self.logfile = None
+        self.every = every
     
     def c_pt(self, c):
         """give a pygame point from complex point"""
@@ -69,6 +71,7 @@ class Car(gymnasium.Env):
         W1 = self.project(self.current[0], self.current[1], self.dist/2)
         W2 = self.project(self.current[0], self.current[1], -self.dist/2)
         pygame.draw.circle(self.screen, (0, 0, 0), (self.misize, self.misize), self.eps)
+        pygame.draw.circle(self.screen, (0, 0, 255), self.c_pt(self.current[0]), 2)
         pygame.draw.line(
             self.screen,
             (0, 0, 255),
@@ -82,18 +85,24 @@ class Car(gymnasium.Env):
     def clipVel(self, vel):
         return max(min(vel, self.maxSpeed), -self.maxSpeed)
 
+    def get_obs(self):
+        length = abs(self.current[0])
+        return get_arr(length/self.maxi, cmath.log(self.current[0]/length).imag/math.pi, self.curVel[0]/self.maxSpeed, self.curVel[1]/self.maxSpeed)
+
     def step(self, acc):
         #acc1, acc2 = acc
         #comPenality = (acc1**2+acc2**2)*10
+        assert abs(acc).max() <= 1.01
+        #acc += np.random.randn(2)/100
         self.curVel += acc*self.refresh
         self.curVel = np.array(list(map(self.clipVel, self.curVel)), dtype=np.float64)
         if self.curVel[0] == self.curVel[1]:
             self.current[0] += cmath.exp((self.current[1]+math.pi/2)*1j)*self.curVel[0]*self.refresh
         else:
-            v1, v2 = self.curVel*self.refresh
+            v1, v2 = self.curVel*self.refresh#*self.radius
             w = (v1-v2)/self.dist
             r1 = v1/w
-            #r2 = r1-self.dist
+            r2 = r1-self.dist
             C = self.current[0]+(self.dist/2-r1)*cmath.exp(1j*self.current[1])
             #print(r1, C, self.current[0], abs(self.current[0]))
             Ap = C+(self.current[0]-C)*cmath.exp(1j*w)
@@ -102,25 +111,30 @@ class Car(gymnasium.Env):
         distA = abs(self.current[1])
         self.counter += 1
         if dist < self.eps:# and distA < self.epsAngle:
-            self.somreward += 1
-            return self.get_obs(), 1000, True, False, {}
-        if dist > self.maxi or self.counter > self.limitStep:
-            self.somreward -= 1
-            return self.get_obs(), -1, False, True, {}
-        #return self.get_obs(), -(dist+distA*10/(2*math.pi)), False, False, {}
-        #reward = math.exp(-dist**2/self.mimax**2)#-comPenality
-        reward = 1-dist**2/self.maxi**2
+            self.somreward += self.limitStep
+            return self.get_obs(), self.limitStep, True, False, {}
+        if dist > self.maxi:
+            self.somreward -= self.limitStep
+            return self.get_obs(), -self.limitStep, False, True, {}
+        if self.counter > self.limitStep:
+            self.somreward -= 2
+            return self.get_obs(), -2, False, True, {}
+        reward = math.exp(-dist**2/self.mimax**2)-1
+        #reward = 1-dist**2/self.maxi**2
+        #reward = math.exp(self.lastDist-dist)-1 if self.lastDist < dist else dist-self.lastDist
+        #reward = -dist**2/self.maxi**2
         self.lastDist = dist
         self.somreward += reward
         return self.get_obs(), reward, False, False, {}
 
-    def get_obs(self):
-        return get_arr(self.current[0].real/250, self.current[0].imag/250, self.current[1]/math.pi-1, self.curVel[0]/self.maxSpeed, self.curVel[1]/self.maxSpeed)
-
     def reset(self, seed=None):
         if seed is not None:
             random.seed(seed)
-        self.current = [(random.random()-0.5)*self.maxi+(random.random()-0.5)*self.maxi*1j, random.random()*2*math.pi]
+        rA = random.random()*2*math.pi
+        while True:
+            L = (random.random()-0.5)*2*self.stLengthMax
+            if L > self.eps:break
+        self.current = [self.project(0j, rA, L), random.random()*2*math.pi]
         self.curVel = np.array([0, 0], dtype=np.float64)
         self.lastDist = abs(self.current[0])
         if hasattr(self, 'somreward') and self.logfile is not None:
@@ -136,7 +150,7 @@ class Car(gymnasium.Env):
         if isinstance(render_mode, str):
             render_mode = render_mode == 'human'
         assert render_mode is self.render_mode
-        if self.render_mode:
+        if self.render_mode and self.counter%self.every == self.every-1:
             self.draw()
 
     def close(self):
@@ -149,12 +163,43 @@ def make_env(id):
     return _init
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == "test":
+    if len(sys.argv) > 1 and sys.argv[1] == "exact":
         env = Car(render_mode='human')
+        random.seed(42)
         while True:
-            ns, r, done, trunc, info = env.step(np.array((0.001, 0.0)))
-            assert not (done or trunc)
-            env.counter %= 10
+            start, info = env.reset()
+            length = start[0]*env.maxi
+            l = length/2
+            a = math.fmod(start[1]*math.pi+math.pi, math.pi*2)
+            m = l/math.cos(a)
+            r = l/m
+            acc = np.array([(m+env.dist/2)*r, (m-env.dist/2)*r])
+            acc /= abs(acc).max()
+            stacc = acc.copy()
+            print(acc, m, env.current[1], a, env.lastDist, length, r)
+            while True:
+                ns, r, done, trunc, info = env.step(acc)
+                env.counter %= 100
+                env.render()
+                
+                assert abs(ns[2]/ns[3]-stacc[0]/stacc[1]) < 0.01, (acc, ns)
+                if max(abs(ns[2:]*env.maxSpeed+acc*env.refresh)) > env.maxSpeed:
+                    acc = np.array([0, 0], dtype=np.float64)
+                #assert not (done or trunc)
+                if done:break
+        sys.exit()
+    elif len(sys.argv) > 1 and sys.argv[1] == 'test':
+        random.seed(42)
+        env = Car(render_mode='human', every=1)
+        u = 0.1
+        for command in np.array([[-u, u], [u, 0.0], [0.0, -u], [u, u*2], [u, u]]):
+            env.reset()
+            while True:
+                ns, r, done, trunc, info = env.step(command)
+                env.render()
+                if trunc:break
+            print(env.counter)
+        sys.exit()
     from stable_baselines3 import PPO
     from stable_baselines3.common.evaluation import evaluate_policy
     from stable_baselines3.common.env_checker import check_env
@@ -164,11 +209,11 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         #parEnv = make_vec_env(env, n_envs=2)
         policy_kwargs = dict(activation_fn=torch.nn.Tanh,
-                        net_arch=dict(pi=[128, 128], vf=[128, 128]))
+                        net_arch=dict(pi=[16, 16], vf=[16, 16]))
         nb_envs = 6
         vecEnv = SubprocVecEnv([make_env(id) for id in range(nb_envs)])
-        model = PPO("MlpPolicy", vecEnv, policy_kwargs=policy_kwargs, verbose=1, learning_rate=0.03)
-        model.learn(total_timesteps=int(2e5), progress_bar=True)
+        model = PPO("MlpPolicy", vecEnv, policy_kwargs=policy_kwargs, verbose=1, learning_rate=0.003)
+        model.learn(total_timesteps=int(2e6), progress_bar=True)
         model.save('ppo_car.zip')
         env = Car(render_mode='human')
         plt.ion()
@@ -185,7 +230,6 @@ if __name__ == '__main__':
     else:
         model = PPO.load(sys.argv[1])
         env = Car(render_mode='human')
-        env.limitStep *= 10
     mean_reward, std_reward = evaluate_policy(model, Monitor(env), render=True, n_eval_episodes=100)
     print(f"{mean_reward}±{std_reward}")
     vec_env = env
